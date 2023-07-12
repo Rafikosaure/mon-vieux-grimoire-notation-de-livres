63eec02d8c7c6b00d423e6034bccd60091110f98
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-07-12 19:46:18 +0200

    Création de notre fichier "app.js" qui contiendra l'application centrale du projet, avec le système de connexion à la base de données MongoDB ainsi que le middleware de prévention des erreurs CORS pour toutes les requètes du projet.

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,46 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+app.use(express.json())
+const mongoose = require('mongoose')
 
-const app = express();
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+mongoose
+    .connect(
+        'mongodb+srv://rafikbensadi:PaaOckOm0obzGWgQ@cluster-monvieuxgrimoir.betoteu.mongodb.net/?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+})
 
-module.exports = app;
+app.use((req, res, next) => {
+    console.log('Requête reçue !')
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(201)
+    next()
+})
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' })
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !')
+})
+
+module.exports = app
