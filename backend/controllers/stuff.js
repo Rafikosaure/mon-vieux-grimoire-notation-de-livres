78bc09af6c5fcfe97c303e8ae7997c30b9a25072
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-07-19 10:21:00 +0200

    Implémentation de la route POST de notation d'un livre et de calcul de la moyenne des notations pour ce livre.

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,18 +1,23 @@
+const { error } = require('console')
+const book = require('../models/book')
 const Book = require('../models/book')
 const fs = require('fs')
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book)
-    console.log(bookObject)
+    // console.log(bookObject)
     delete bookObject._id
     delete bookObject._userId
+    // Ici: code sharp !
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
+        // imageUrl: ref(sharp)
         imageUrl: `${req.protocol}://${req.get('host')}/images/${
             req.file.filename
         }`,
     })
+    // console.log(book)
     book.save()
         .then(() => {
             res.status(201).json({ message: 'Livre enregistré !' })
@@ -65,6 +70,40 @@ exports.modifyBook = (req, res, next) => {
         })
 }
 
+exports.giveARating = (req, res, next) => {
+    const { userId, rating } = req.body
+
+    // Trouver le livre à noter
+    Book.findOne({ _id: req.params.id }).then((book) => {
+        
+        // Vérification de l'identité de l'utilisateur
+        if (userId !== req.auth.userId) {
+            res.status(400).json({ message: 'Not authorized !' })
+        } else {
+
+            // Ajout de la nouvelle notation dans le tableau
+            const newRating = {
+                userId: userId,
+                grade: rating,
+            }
+            book.ratings.push(newRating)
+
+            // Calcul de la moyenne des notations
+            const sumGrades = book.ratings
+                .map((rating) => rating.grade)
+                .reduce((prev, curr) => prev + curr, 0)
+            const floatAverageRating = sumGrades / book.ratings.length
+            const averageRating = Math.round(floatAverageRating)
+            book.averageRating = averageRating
+
+            // Enregistrement des changements
+            book.save()
+            .then(() => res.status(200).json({ book }))
+            .catch((error) => res.status(401).json({ error }))
+        }
+    })
+}
+
 exports.deleteOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then((book) => {
@@ -76,7 +115,7 @@ exports.deleteOneBook = (req, res, next) => {
                     Book.deleteOne({ _id: req.params.id })
                         .then(() => {
                             res.status(200).json({
-                                message: 'Livre supprimé !',
+                                message: 'Livre supprimé !'
                             })
                         })
                         .catch((error) => res.status(401).json({ error }))
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -8,20 +8,7 @@ router.post('/', auth, multer, stuffCtrl.createBook)
 router.get('/', stuffCtrl.getAllBooks)
 router.get('/:id', stuffCtrl.getOneBook)
 router.put('/:id', auth, multer, stuffCtrl.modifyBook)
+router.post('/:id/rating', auth, stuffCtrl.giveARating)
 router.delete('/:id', auth, stuffCtrl.deleteOneBook)
-
-// router.get((req, res, next) => {
-//     console.log('Requête reçue !')
-//     next()
-// })
-
-// router.put((req, res, next) => {
-//     res.json({ message: 'Votre requête a bien été reçue !' })
-//     next()
-// })
-
-// router.delete((req, res, next) => {
-//     console.log('Réponse envoyée avec succès !')
-// })
 
 module.exports = router
