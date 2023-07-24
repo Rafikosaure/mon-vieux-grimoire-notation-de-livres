c47ae48cfe0f4c2cc82bab8f8b00e187cdf85409
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-07-24 16:42:05 +0200

    Modification de l'ordre des routes pour un test.

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -5,10 +5,10 @@ const multer = require('../middleware/multer-config')
 const router = express.Router()
 
 router.get('/bestrating', stuffCtrl.getBestRatedBooks)
+router.put('/:id', auth, multer, stuffCtrl.modifyBook)
 router.post('/', auth, multer, stuffCtrl.createBook)
 router.get('/', stuffCtrl.getAllBooks)
 router.get('/:id', stuffCtrl.getOneBook)
-router.put('/:id', auth, multer, stuffCtrl.modifyBook)
 router.post('/:id/rating', auth, stuffCtrl.giveARating)
 router.delete('/:id', auth, stuffCtrl.deleteOneBook)
 
