3db3d58c75adaa57a332e5e7d911e13c1912c78b
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2023-07-25 11:00:20 +0200

    Nettoyage du code du fichier multer-config.js (retrait des commentaires inutiles et formatage avec Prettier).

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,35 +1,22 @@
 const multer = require('multer')
 
-// *************** Méthode 1: Enregistrement dans diskStorage() ***************
-
-// const MIME_TYPES = {
-//     'image/jpg': 'jpg',
-//     'image/jpeg': 'jpg',
-//     'image/png': 'png'
-// }
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//         callback(null, 'images')
-//     },
-//     filename: (req, file, callback) => {
-//         const name = file.originalname.split(' ').join('_')
-//         const extension = MIME_TYPES[file.mimetype]
-//         callback(null, name + Date.now() + '.' + extension)
-//     }
-// })
-
-// *************** Méthode 2: Enregistrement dans memoryStorage() ***************
-
+// Enregistrement de l'image chargée dans la mémoire
 const storage = multer.memoryStorage()
 
+// Fonction de filtrage du fichier chargé selon son extension
 const multerFilter = (req, file, callback) => {
     console.log(file.mimetype)
-    if (file.mimetype.split('/')[1] !== 'jpg' && file.mimetype.split('/')[1] !== 'jpeg' && file.mimetype.split('/')[1] !== 'png') {
+    if (
+        file.mimetype.split('/')[1] !== 'jpg' &&
+        file.mimetype.split('/')[1] !== 'jpeg' &&
+        file.mimetype.split('/')[1] !== 'png'
+    ) {
         callback(new Error('Fichier non-conforme !'), false)
     } else {
         callback(null, true)
     }
 }
 
-module.exports = multer({ storage: storage, fileFilter: multerFilter }).single('image')
+module.exports = multer({ storage: storage, fileFilter: multerFilter }).single(
+    'image'
+)
