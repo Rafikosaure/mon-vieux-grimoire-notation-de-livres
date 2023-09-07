903ea175d1ac68767177fb966784a2e65f98aae8
rafik <rafikbensadi@live.fr>
2023-09-07 10:43:12 +0200

    Test pour le déploiement du site sur Vercel.

--- P7-Dev-Web-livres/src/utils/constants.js
+++ P7-Dev-Web-livres/src/utils/constants.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://monvieuxgrimoireapi.vercel.app';
+// 'http://localhost:4000';
+
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
