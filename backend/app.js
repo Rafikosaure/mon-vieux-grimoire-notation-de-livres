e61fcbe5fcf93875d19070f7cfb84953bcc2c650
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-19 04:40:34 +0200

    Corrections importantes pour le déploiement.

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ mongoose
 
 /** Gestion des erreurs CORS */
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_ENDPOINT)
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
